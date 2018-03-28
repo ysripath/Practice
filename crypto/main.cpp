1c24304500c4b364168fa54a29e27c68f934ba48
Yuvaraj <yuvarajsripathi91@gmail.com>
2018-03-28 07:09:37 +0000

    mend

--- crypto/main.cpp
+++ crypto/main.cpp
@@ -145,10 +145,24 @@ bool verifySignature(std::string publicKey, std::string plainText, char* signatu
 }
 
 
+std::string& fix_newlines(std::string& s)
+{
+    size_t start_pos = 0;
+    while((start_pos = s.find("\\n", start_pos)) != std::string::npos) {
+         s.replace(start_pos, 2, "\n");
+         start_pos += 1;
+    }
+    return s;
+}
 
 int main()
 {
-  std::string plainText = "Test message";
+	std::ifstream iF("message.txt");
+	//iF.open();
+  //std::string plainText = "9b27ea15; 0; ; 1; (Alice, 5000)\n";
+	std::string plainText;// = "0; ; 1; (Alice, 5000)\n";
+	getline(iF, plainText);
+	plainText += "\n";	
   //std::string privateKey = "";
 
   //ifstream infile { "private.pem" };
@@ -161,11 +175,31 @@ std::string privateKey((std::istreambuf_iterator<char>(t)),
 
   RSA* privateRSA = createPrivateRSA(privateKey);
   unsigned char* encMessage;
-  char* base64Text;
+  char* base64Text; // = "fFZ5j4MEXF7eIKiWYpe+sE+sBQTy241Jy3lxZdNHqhdljA7vBNu2LZaDr8JfXIq7\npUqZSMDkO6KKhClDInnXbDkAq4F7WXu4EV4xQJUb+CX2foW0l/9lSdM64w1ui+jB\nSSmeTwlnqDaoSMbiQyzx5yFSZLGUCmWxdcPNIMzek4h8qOU0FNBGGEOqjNhP94/y\nmMYXxuRnZq4JtSEiKAKtasX7QOxWnLwYRifulQUNNwIOP2ro2VdXJYtH8UK9K0Sv\nu1CiOhM4IBBG1Q1o8BXe2VZGfix5OLCvB7Vu+MBpakqg9yC557aJLkoRVj+c39yD\nZOvgUbspUb37/1CsyaEL0w==\n";
+ std::ifstream iS("sign.txt");
+	std::string base64;
+	getline(iS, base64);
+	base64 = fix_newlines(base64);
+	std::cout<<"Base - "<<base64<<std::endl;
+	base64Text = (char*)base64.c_str();
+
+// = "qXJKCyq95OEmB/L90jyY+ve3mJy3eeGiVeEaa1sBZoJeCuhVrHzXw3azPWvDQYxt\nPZz4QZynpiNewftHUkK+R52ZcIZ4Qzzvx3d6tjMUwiebuVYVLDDBvgsCBlx/94HI\naIN4Ngj8pTYHXdVxokrA32Yxz6PuFK2Xl+aqoDKvDckxhMX8KyRgaqc6W5Zq/+0t\ngcQOXcxTsDwp5AG0mOR1NyllKaCjjmpcdfQt3nhmrJq+TSzb++6KmcpUFqAR7pzM\npUVbvlxF1gb77sglhY3aYzbDXhBad5vYXiWJj0F3O5J3ZoVdHHjZNoHux5bUSRYq\n/oRmZ5BbtAh1SZjR709a0Q==\n";
+	/*std::string tempS = "qXJKCyq95OEmB/L90jyY+ve3mJy3eeGiVeEaa1sBZoJeCuhVrHzXw3azPWvDQYxtPZz4QZynpiNewftHUkK+R52ZcIZ4Qzzvx3d6tjMUwiebuVYVLDDBvgsCBlx/94HIaIN4Ngj8pTYHXdVxokrA32Yxz6PuFK2Xl+aqoDKvDckxhMX8KyRgaqc6W5Zq/+0tgcQOXcxTsDwp5AG0mOR1NyllKaCjjmpcdfQt3nhmrJq+TSzb++6KmcpUFqAR7pzMpUVbvlxF1gb77sglhY3aYzbDXhBad5vYXiWJj0F3O5J3ZoVdHHjZNoHux5bUSRYq/oRmZ5BbtAh1SZjR709a0Q==";
+	char base64Text[1000];
+	strcpy(base64Text, tempS.c_str());*/
+
+/* qXJKCyq95OEmB/L90jyY+ve3mJy3eeGiVeEaa1sBZoJeCuhVrHzXw3azPWvDQYxt
+PZz4QZynpiNewftHUkK+R52ZcIZ4Qzzvx3d6tjMUwiebuVYVLDDBvgsCBlx/94HI
+aIN4Ngj8pTYHXdVxokrA32Yxz6PuFK2Xl+aqoDKvDckxhMX8KyRgaqc6W5Zq/+0t
+gcQOXcxTsDwp5AG0mOR1NyllKaCjjmpcdfQt3nhmrJq+TSzb++6KmcpUFqAR7pzM
+pUVbvlxF1gb77sglhY3aYzbDXhBad5vYXiWJj0F3O5J3ZoVdHHjZNoHux5bUSRYq
+/oRmZ5BbtAh1SZjR709a0Q==*/
+
+// = "nLCn8A1gxJZoKO48sa0ieWGdCMsW5Vt0gXbJPyXVZuDAOmgsGcS8YhoSjvuEFegrtjUGEUbyJOigkCePa1ouBnWP8KdCeOWbx92aoVQfZ24NR6pM0mC0dcudYyzJM97qO7Y0NoW5QLPPNWEY13y+2lTVeiZ26uvfY8bLvAosouY36IRuxu4eH+E/5aYnvuA4UEz0yVaXWOj0gvuwzuGrLnDz8n7dKnQeHZzcqiaQ8WyFWoJy+bwgGHQwqdBWNl0yPwygu9E1quCyK1L+LGkpPT5j3mzR48ubXBw7I9BbYxYjfhjRt7iTKFTp+5UbUPOp2OsrvszkF/jNv9DjJ3igYA==";
   size_t encMessageLength;
   RSASign(privateRSA, (unsigned char*) plainText.c_str(), plainText.length(), &encMessage, &encMessageLength);
-  Base64Encode(encMessage, encMessageLength, &base64Text);
-  free(encMessage);
+ // Base64Encode(encMessage, encMessageLength, &base64Text);
+  	free(encMessage);
   //return base64Text;
 	std::cout<<"Base64-> "<<base64Text<<"\n";
 
